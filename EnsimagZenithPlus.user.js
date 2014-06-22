17756439af825db2a55c01375cb5ec5091bea000
Arthur Sonzogni <Arthur.Sonzogni@ensimag.grenoble-inp.fr>
2014-06-22 18:04:14 +0200

    tableau éditable et mise-à-jour

--- EnsimagZenithPlus.user.js
+++ EnsimagZenithPlus.user.js
@@ -36,10 +36,17 @@ var ZP = {
 
         // methodes
         run : function() {
-            this.calculMoyenne();
-            this.addInformationPanel();
+            this.cleanInterface();
+            this.updateView();
             this.checkNewElement();
             this.addLineColor();
+            //this.cascadeApparition();
+            this.autoriserModifications();
+        },
+
+        updateView : function() {
+            this.calculMoyenne();
+            this.addInformationPanel();
         },
 
         calculMoyenne : function() {
@@ -61,8 +68,13 @@ var ZP = {
         },
 
         addInformationPanel : function() {
-            $("table").append("<h2><strong>Moyenne générale = "+this.moyenne.toFixed(2)+"</strong></h2>");
-            $("table").append("<h3>(Ecart-type = "+this.ecartType.toFixed(2)+")</h3>");
+            // suppression de l'ancien panneau
+            $(".panelAdded").hide('slow',function(){
+                $(this).remove();
+            });
+            // ajout du nouveau
+            $("table").append("<h2 class='panelAdded'><strong>Moyenne générale = "+this.moyenne.toFixed(2)+"</strong></h2>");
+            $("table").append("<h3 class='panelAdded'>(Ecart-type = "+this.ecartType.toFixed(2)+")</h3>");
         },
 
 
@@ -70,13 +82,12 @@ var ZP = {
             $("tbody tr").each( function(){
                 var tds = $(this).children("td");
                 var note = parseFloat(tds.eq(3).text());
-                if      (note<8)     $(this).css("background-color","#FF3333");
+                if      (note<8)    $(this).css("background-color","#FF3333");
                 else if (note<10)   $(this).css("background-color","#FFAAAA");
                 else if (note<12)   $(this).css("background-color","#FFB981");
                 else if (note<14)   $(this).css("background-color","#CCFFA9");
                 else if (note<16)   $(this).css("background-color","#76FF76");
                 else                $(this).css("background-color","#009953");
-
             });
 
 
@@ -98,8 +109,48 @@ var ZP = {
                     }
                 });
             }
-        }
+        },
+
+        cascadeApparition : function() {
+            $('tbody tr').hide();
+            $('tbody tr').first().show('fast', function showNextOne() {
+                $(this).next('tr').show(50, showNextOne);
+            });  
+        },
         
+        autoriserModifications : function() {
+            var that = this;
+            $("tbody td").click(function () {
+                var OriginalContent = $(this).text();
+
+                $(this).addClass("cellEditing");
+                $(this).html("<input type='text' value='" + OriginalContent + "' />");
+                $(this).children().first().focus();
+
+                $(this).children().first().keypress(function (e) {
+                    if (e.which == 13) {
+                        var newContent = $(this).val();
+                        $(this).parent().text(newContent);
+                        $(this).parent().removeClass("cellEditing");
+                        that.updateView();
+                    }
+                });
+
+                $(this).children().first().blur(function(){
+                    $(this).parent().text(OriginalContent);
+                    $(this).parent().removeClass("cellEditing");
+                    that.updateView();
+                });
+            });
+        },
+        
+        cleanInterface : function() {
+            //$("#main").children().eq(0).remove();
+            //$("#main").children().eq(1).remove();
+            //$("#main").contents().filter(function() {
+                    //return this.nodeType === 3;
+            //}).remove();
+        },
         
 
     },
