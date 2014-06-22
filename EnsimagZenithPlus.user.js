52fb7b5aaf2cd51ff03695c4581f378e26223c0b
Arthur Sonzogni <Arthur.Sonzogni@ensimag.grenoble-inp.fr>
2014-06-22 16:57:35 +0200

    ajout detection nouvelle element

--- EnsimagZenithPlus.user.js
+++ EnsimagZenithPlus.user.js
@@ -32,17 +32,19 @@ var ZP = {
 
         // attribut
         moyenne : 0.0,
-
+        ecartType : 0.0,
 
         // methodes
         run : function() {
-            this.addLineColor();
             this.calculMoyenne();
             this.addInformationPanel();
+            this.checkNewElement();
+            this.addLineColor();
         },
 
         calculMoyenne : function() {
             var sumNote = 0;
+            var sumNote2 = 0;
             var sumCoef = 0;
             $("tbody tr").each( function(){
 
@@ -50,23 +52,25 @@ var ZP = {
                 var coef = parseFloat(tds.eq(1).text());
                 var note = parseFloat(tds.eq(3).text());
 
+                sumNote2 += coef * note * note;
                 sumNote += coef * note;
                 sumCoef += coef;
             });
             this.moyenne = sumNote/sumCoef;
+            this.ecartType = Math.sqrt(sumNote2-sumNote)/sumCoef;
         },
 
-        addInformationPanel : function()
-        {
+        addInformationPanel : function() {
             $("table").append("<h2><strong>Moyenne générale = "+this.moyenne.toFixed(2)+"</strong></h2>");
+            $("table").append("<h3>(Ecart-type = "+this.ecartType.toFixed(2)+")</h3>");
         },
 
-        addLineColor : function()
-        {
+
+        addLineColor : function() {
             $("tbody tr").each( function(){
                 var tds = $(this).children("td");
                 var note = parseFloat(tds.eq(3).text());
-                if (note<8)         $(this).css("background-color","#FF3333");
+                if      (note<8)     $(this).css("background-color","#FF3333");
                 else if (note<10)   $(this).css("background-color","#FFAAAA");
                 else if (note<12)   $(this).css("background-color","#FFB981");
                 else if (note<14)   $(this).css("background-color","#CCFFA9");
@@ -74,8 +78,28 @@ var ZP = {
                 else                $(this).css("background-color","#009953");
 
             });
+
+
+            $("table td").css({padding:"10px"});
         },
 
+        checkNewElement : function() {
+            if (typeof(Storage) != "undefined")
+            {
+                //localStorage.clear();
+                $("tbody tr").each( function(){
+                    var tds = $(this).children("td");
+                    var matiere = tds.eq(0).text();
+                    var note = tds.eq(3).text();
+                    if (localStorage.getItem(matiere) !== note) {
+                        tds.eq(0).append('<span style="color:#990000">  ( Nouveau ) </span> ');
+                        $(this).css("font-weight","Bold");
+                        localStorage.setItem(matiere,note);
+                    }
+                });
+            }
+        }
+        
         
 
     },
